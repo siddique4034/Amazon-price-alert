441e44ad3e4290451c016506f0fdf03e7da84b0d
Muhammad Siddique <97290781+siddique4034@users.noreply.github.com>
2023-12-29 22:19:28 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -8,14 +8,14 @@ PASSWORD = os.environ['PASSWORD'] #You have to provide your app password here
 
 class SendMail:
    def __init__ (self, product_price, product_link, product_title):
-     self.message = f'Subject: Price Alert🚨\n\nThe price of "{product_title}" has dropped to ₹{product_price}\n\n Link 🔗 of Product: {product_link}'.encode("utf-8")
+      self.message = f'Subject: Price Alert🚨\n\nThe price of "{product_title}" has dropped to ₹{product_price}\n\n Link 🔗 of Product: {product_link}'.encode("utf-8")
     
-     with smtplib.SMTP("smtp.gmail.com") as connection:
-       connection.starttls()
-       connection.login(user=MY_EMAIL, password=PASSWORD)
-       connection.sendmail(
-         from_addr=MY_EMAIL, 
-         to_addrs=TO_EMAIL, 
-         msg=self.message)
+      with smtplib.SMTP("smtp.gmail.com") as connection:
+         connection.starttls()
+         connection.login(user=MY_EMAIL, password=PASSWORD)
+         connection.sendmail(
+            from_addr=MY_EMAIL, 
+            to_addrs=TO_EMAIL, 
+            msg=self.message)
 
 
